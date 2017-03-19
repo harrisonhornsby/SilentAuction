6c34753c1a2b7ecb014ed1a237e188176982f260
Harrison Hornsby <harrisonhornsby@www3.g18.pair.com>
2017-03-18 20:45:28 -0400

    Add/modify/view for donors and items, print center

---
+++ Part1/PrintCenter.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: harri
+ * Date: 3/18/2017
+ * Time: 8:11 PM
+ */
+
+?>
+<html>
+<head>
+    <script type="text/javascript" src="js/jquery-1.8.0.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+
+<body>
+
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="Index.html">Silent Auction</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li><a href="Donors.php">Donors</a></li>
+                <li><a href="Items.php">Items</a></li>
+                <li class="active"><a href="PrintCenter.php">Print Center</a></li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
+
+<div class="container">
+    <button type="button" class="btn btn-primary btn-lg btn-block"onclick="window.open('DonationRequest.pdf')">Print Donation Request</button>
+    <button type="button" class="btn btn-primary btn-lg btn-block">Generate Tax Receipt</button>
+</div>
+
+</body>
+
+</html>
